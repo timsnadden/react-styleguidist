390f15bb9a7cf5caea3cd86b4797e98b0cb67db3
Tim Snadden <tim@snadden.com>
2017-04-26 20:11:46 +1200

    Add pink to show issue with jss-isolate

--- examples/basic/styleguide.config.js
+++ examples/basic/styleguide.config.js
@@ -2,6 +2,9 @@ const loaders = require('loaders');
 module.exports = {
 	components: 'src/components/**/[A-Z]*.js',
 	defaultExample: true,
+	theme: {
+		base: 'pink',
+	},
 	webpackConfig: {
 		module: {
 			loaders: loaders.all,
